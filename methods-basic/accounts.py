ae7d33a9e89fc674eaa109c2809b30902cbc4e7a
Aditi Phadnis <aditi.phadnis@gmail.com>
2025-02-10 19:53:37 +0530

    Changes made to accounts.py

--- methods-basic/accounts.py
+++ methods-basic/accounts.py
@@ -14,27 +14,21 @@ class Account:
     def __init__(self, name, balance=0):
         self.name = name
         self.balance = balance
-        self._transaction_list = [Account._current_time(), balance]
+        self._transaction_list = []
         print(f"Account created for {self.name}")
-        self.show_balance()
 
     def deposit(self, amount):
         if amount > 0:
             self.balance += amount
-            print(f"{amount} is deposited")
             self.show_balance()
-            self.transaction_list.append((Account._current_time(), amount))   
-        return self.balance
+            self._transaction_list.append((pytz.utc.localize(datetime.datetime.utcnow(), amount)))   
     
     def withdraw(self, amount):
         if 0 < amount <= self.balance:
             self.balance -= amount
-            print(f"{amount} is withdrawn")
-            self.show_balance()
-            self.transaction_list.append((Account._current_time(), -amount))
         else:
             print(f"Amount must be greater than zero and no more than your account balance")
-        return self.balance
+        self.show_balance()
     
     def show_balance(self):
         print(f"Balance is {self.balance}")
@@ -56,18 +50,6 @@ if __name__ == "__main__":
     aditi = Account("Aditi", 0)
     aditi.deposit(1000)
     aditi.withdraw(500)
-    # aditi.show_balance()
     aditi.withdraw(2000)
-    # aditi.show_balance()
-    aditi.deposit(2000)
-    # aditi.show_balance()
-    aditi.withdraw(2000)
-    # aditi.show_balance()
-    aditi.show_transactions()
-
-    steph = Account("Steph", 1000)
-    steph.deposit(100)
-    steph.withdraw(300)
-    steph.show_transactions()
 
 
